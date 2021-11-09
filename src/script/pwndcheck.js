25e4b959096c4ebea298bf03137c4235cd6c04af
Florian Thiévent <florianjeremias@gmail.com>
2021-11-09 17:15:53 +0100

    added email from breachdirectory

---
+++ src/script/pwndcheck.js
@@ -0,0 +1,7 @@
+/**
+ * Hi Florian,
+ Here's your free API key: 918b8543267f8baceeee4eff15b2ee7d
+ The API documentation can be found here: https://breachdirectory.com/api
+ With the API key, users are also able to log in to the User Control Panel to use more features (the UserCP is in development and in active beta now, but it can still be used): https://breachdirectory.com/usercp
+ Thanks for using the service!
+ */
