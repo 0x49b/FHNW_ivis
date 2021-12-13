0ecc36e758d7e45b538a2319ffa26b8600c8915b
Florian <florianjeremias@gmail.com>
2021-12-13 17:23:44 +0100

    added new graph for ages

--- assets/bar_chart.js
+++ assets/bar_chart.js
@@ -1,49 +1,47 @@
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin = {top: 10, right: 30, bottom: 250, left: 50},
+    width = 400 - margin.left - margin.right,
+    height = 1000 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#bar_chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .attr("transform", 'rotate(90deg)')
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Parse the Data
-d3.csv("data/ages.csv").then( function(data) {
+d3.csv("data/ages.csv").then(function (data) {
 
     // List of subgroups = header of the csv files = soil condition here
-    const subgroups = data.columns.slice(1)
+    const subgroups = data.columns.slice(1);
 
 
     // List of groups = species here = value of the first column called group -> I show them on the X axis
-    const groups = data.map(d => (d.group))
+    const groups = data.map(d => (d.group));
 
     // Add X axis
     const x = d3.scaleBand()
         .domain(groups)
         .range([0, width])
-        .padding([0.2])
+        .padding([0.1])
     svg.append("g")
         .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0));
+        //.call(d3.axisBottom(x).tickSizeOuter(0));
+        .call(d3.axisBottom(x).tickSize(0));
 
     // Add Y axis
     const y = d3.scaleLinear()
         .domain([0, 2500])
-        .range([ height, 0 ]);
-    svg.append("g")
+        .range([height, 0]);
+    /*svg.append("g")
         .call(d3.axisLeft(y));
-
+    */
     // color palette = one color per subgroup
-
-
-const color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#e41a1c','#377eb8','#4daf4a'])
+    const color = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#000000', '#212121', '#727272', '#9a9999', '#e0e0e0'])
 
     //stack the data? --> stack per subgroup
     const stackedData = d3.stack()
@@ -65,6 +63,6 @@ const color = d3.scaleOrdinal()
         .attr("x", d => x(d.data.group))
         .attr("y", d => y(d[1]))
         .attr("height", d => y(d[0]) - y(d[1]))
-        .attr("width",x.bandwidth())
+        .attr("width", x.bandwidth())
 
 })
