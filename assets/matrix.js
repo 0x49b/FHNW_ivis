5ebeb41f9585e63a1e579b44269fd3da8b0cf945
Florian Thiévent <hi@thievent.org>
2022-01-05 21:53:17 +0100

    fixed first screen

--- assets/matrix.js
+++ assets/matrix.js
@@ -1,17 +1,50 @@
 const canvas = document.getElementById('canv');
 const ctx = canvas.getContext('2d');
 
+const section = document.getElementsByClassName("fp-tableCell")[0]
+
 let w = canvas.width = document.body.offsetWidth;
-let h = canvas.height = document.body.offsetHeight;
+//let h = canvas.height = document.body.offsetHeight;
+let matrixHeight = canvas.height = section.getBoundingClientRect().height;
+
+console.log(section.getBoundingClientRect().height);
+
 const cols = Math.floor(w / 20) + 1;
 const ypos = Array(cols).fill(0);
 
 ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, w, h);
+ctx.fillRect(0, 0, w, matrixHeight);
+
+
+function roundedRect(ctx, options) {
+    ctx.strokeStyle = options.color;
+    ctx.fillStyle = options.color;
+    ctx.lineJoin = "round";
+    ctx.lineWidth = options.radius;
+
+    ctx.strokeRect(
+        options.x + (options.radius * .5),
+        options.y + (options.radius * .5),
+        options.width - options.radius,
+        options.height - options.radius
+    );
+
+    ctx.fillRect(
+        options.x + (options.radius * .5),
+        options.y + (options.radius * .5),
+        options.width - options.radius,
+        options.height - options.radius
+    );
+
+    ctx.stroke();
+    ctx.fill();
+}
+
 
 function matrix() {
+
     ctx.fillStyle = 'rgba(246,220,0,0.1)';
-    ctx.fillRect(0, 0, w, h);
+    ctx.fillRect(0, 0, w, matrixHeight);
 
     ctx.fillStyle = '#000';
     ctx.font = '15pt monospace';
@@ -23,6 +56,34 @@ function matrix() {
         if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
         else ypos[ind] = y + 20;
     });
+
+    cybercrime();
+}
+
+function cybercrime() {
+
+    roundedRect(ctx, {
+        x: (w / 2) - 300,
+        y: (matrixHeight / 2) - 150,
+        width: 600,
+        height: 300,
+        radius: 40,
+        color: "#000"
+    });
+
+    ctx.font = "100px Patua One";
+    ctx.fillStyle = 'rgba(246,220,0,1.0)';
+    ctx.fillText("Cybercrime", (w / 2) - 250, (matrixHeight / 2) + (110 - 150));
+
+    ctx.font = "20px Montserrat";
+    ctx.fillStyle = 'rgba(246,220,0,1.0)';
+    ctx.fillText("scroll down", (w / 2) - 50, (matrixHeight / 2) + (170 - 150) );
+
+    let arrow = new Image();
+    arrow.onload = function () {
+        ctx.drawImage(arrow, (w / 2) - 15, (matrixHeight / 2) + (200 - 150), 37, 36);
+    };
+    arrow.src = "./assets/arrow_down.svg";
 }
 
 
